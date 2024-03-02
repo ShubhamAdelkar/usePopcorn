d85ab6d5b7bebb4a12fab27eab82699aed6753ca
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-02 21:54:52 +0530

    add cleanup funciton

--- src/App.jsx
+++ src/App.jsx
@@ -111,7 +111,7 @@ export default function App() {
         setError("");
         return;
       }
-
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -333,12 +333,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 
   useEffect(
     function () {
-      document.addEventListener("keydown", function (e) {
+      function callback(e) {
         if (e.code === "Escape") {
           onCloseMovie();
           console.log("Closing movie");
         }
-      });
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
     },
     [onCloseMovie]
   );
