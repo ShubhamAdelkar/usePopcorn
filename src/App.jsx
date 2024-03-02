48933d5b0339d111d9200eac6ab8910ad11e2004
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-02 19:47:29 +0530

    fix racing

--- src/App.jsx
+++ src/App.jsx
@@ -80,12 +80,14 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `https://www.omdbapi.com?apikey=7d57d30b&s=${query}`
+            `https://www.omdbapi.com?apikey=7d57d30b&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -96,7 +98,9 @@ export default function App() {
 
           setMovies(data.Search);
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -109,6 +113,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
