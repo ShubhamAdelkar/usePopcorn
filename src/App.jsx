13aee0791a088e74b76acae81750067b26f87742
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-07 12:41:24 +0530

    add enter key feature

--- src/App.jsx
+++ src/App.jsx
@@ -146,9 +146,21 @@ function Logo() {
 
 function Search({ query, setQuery }) {
   const inputEL = useRef(null);
-  useEffect(function () {
-    inputEL.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEL.current) return;
+        if (e.code === "Enter") {
+          inputEL.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
