a23110081b99131734291f125d7a4ab3fc82d49c
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-21 09:41:11 +0530

    add new custom hook

---
+++ src/useMovie.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const API = "https://www.omdbapi.com?apikey=7d57d30b&s=";
+export function useMovie(query, handleCloseMovie) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(`${API}${query}`, {
+            signal: controller.signal,
+          });
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found"); // if movie name is not make sense.
+
+          setMovies(data.Search);
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      handleCloseMovie();
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+  return { movies, isLoading, error };
+}
